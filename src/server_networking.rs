1de61bc567c9f146b12c5525bee1901181a51f46
Hallgrimur Eggertsson <hallgrimurV@gmail.com>
2023-08-11 20:54:09 +0000

    Testing

--- src/server_networking.rs
+++ src/server_networking.rs
@@ -14,14 +14,13 @@ const MAX_INSTRUCTION_LENGTH: usize = 1024;
 
 pub enum Request {
     Upload,
-    Download,
+    Download(String)
 }
 
-#[derive(FromResidual)]
 pub enum ServerError {
     Utf8(Utf8Error),
     Io(std::io::Error),
-
+    Instruction(InstructionError),
 }
 
 
@@ -41,24 +40,40 @@ impl fmt::Display for InstructionError {
 }
 
 
+pub fn parse_instruction(s: &str) -> Result<Request, InstructionError> {
+    match s {
+        "Sending CSV" => Ok(Request::Upload),
+        _ => Err(InstructionError::Invalid(s.to_owned())),
+    }
+}
+
+
 
 
 pub fn server(address: &str, global: Arc<Mutex<HashMap<String, StrictTable>>>) -> Result<(), Box<dyn Error>> {
     let l = TcpListener::bind(address)?;
 
+    
+    /* This is the main loop of the function. Here we accept incoming connections and process them */
     for stream in l.incoming() {
         println!("Accepted connection");
+
+        // Spawn a new thread for each connection for some semblence of scalability
         let thread_global = global.clone();
         std::thread::spawn(move || {
+
+            /* The first thing the thread does is match on the accepted connection and return a ServerError if there is a problem */
             println!("Spawned thread");
             let mut stream = match stream {
                 Ok(value) => {println!("Unwrapped Result"); value},
                 Err(e) => {return Err(ServerError::Io(e));},
             };
 
+            /* Then we allocate an instruction buffer. It's size should vary according to the incoming transmission. It doesn't currently */
             let mut instructions: [u8; 1024] = [0; 1024];
             println!("Initialized string buffer");
             loop {
+                /* This loop should handle differentl sized transmissions and break when there is no more data */
                 match stream.read(&mut instructions) {
                     Ok(n) => {
                         println!("Read {n} bytes");
@@ -68,37 +83,48 @@ pub fn server(address: &str, global: Arc<Mutex<HashMap<String, StrictTable>>>) -
                 };
             }
             
-            let instruction_string = match str::from_utf8(&instructions) {
-                Ok(value) => value,
+            /* Depending on the instructions received, a different action should be taken */
+            let instruction = match str::from_utf8(&instructions) {
+                Ok(value) => {
+                    println!("{}", value);
+                    value
+                },
                 Err(e) => {return Err(ServerError::Utf8(e));},
             };
 
-            let instruction: Vec<&str> = instruction_string.split('|').collect();
-            let (instruction, buffer_size) = (instruction[0], instruction[1].parse::<usize>()?);
+            let instruction: Vec<&str> = instruction.split('|').collect();
+            if instruction.len() != 2 {
+                return Err(ServerError::Instruction(InstructionError::Invalid(instruction[0].to_owned())));
+            }
+            let (instruction, name) = (instruction[0], instruction[1]);
 
-            if instru{
+            // Here we parse the instructions. I would like to figure out how to make this a function that propagates an InstructionError
+            if instruction == "Sending CSV" {
                 match stream.write("OK".as_bytes()) {
                     Ok(n) => println!("Wrote {n} bytes"),
                     Err(e) => {return Err(ServerError::Io(e));},
                 };
+            } else {
+                stream.write("Invalid request".as_bytes()).expect("Panicked while informing client of invalid request");
+                return Err(ServerError::Instruction(InstructionError::Invalid(instruction.to_owned())));
             }
 
-            stream.flush();
-            println!("Flushed stream");
-
-            let mut csv = String::new();
+            // Here we read the transmitted CSV from the stream into a rust String (aka a Vec)
+            let mut csv = String::from("");
             let b: usize;
             loop {
                 match stream.read_to_string(&mut csv) {
                     Ok(n) => {
                         b = n;
+                        println!("{}", &csv);
                         break;
                     },
                     Err(e) => {return Err(ServerError::Io(e));},
                 };
             }
 
-            match create_StrictTable_from_csv(&csv) {
+            // Here we create a StrictTable from the csv and supplied name
+            match create_StrictTable_from_csv(&csv, name) {
                 Ok(table) => {
                     match stream.write(&b.to_be_bytes()) {
                         Ok(_) => println!("Confirmed correctness with client"),
@@ -112,8 +138,6 @@ pub fn server(address: &str, global: Arc<Mutex<HashMap<String, StrictTable>>>) -
                     Err(e) => {return Err(ServerError::Io(e));},
                 },
             };
-
-            stream.flush();
 
             Ok(())
 
