930c84e20bac90c768ede887fd847084411e5d43
Hallgrimur Eggertsson <hallgrimurV@gmail.com>
2023-09-25 16:26:33 +0000

    cleanup before publish

--- utility_scripts/make_large_csv.py
+++
@@ -1,9 +0,0 @@
-
-printer = "vnr;heiti;magn"
-for i in range(1, 1000000):
-    if i%100 == 0:
-        print(f"Still going, i: {i}")
-    printer += f"i{i};product name;569\n"
-
-with open("large.csv", 'w') as f:
-    f.write(printer)
