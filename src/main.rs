8afb975c6d2fe7016ab63dda95429da99f55dd98
Hallgrimur Eggertsson <hallgrimurV@gmail.com>
2023-09-25 22:50:39 +0000

    checking things

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,9 @@ mod aes_temp_crypto;
 
 fn main() -> Result<(), networking_utilities::ServerError> {
 
+    let current_dir = std::env::current_dir()?;
+    println!("The current directory is {}", current_dir.display());
+
     let global: HashMap<String, db_structure::StrictTable> = HashMap::new();
     let arc_global = Arc::new(Mutex::new(global));
     server_networking::server("127.0.0.1:3004", arc_global.clone())?;
