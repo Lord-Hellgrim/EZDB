072bbca9f5f3f918619dbc7f03f54607dec59233
Hallgrímur Eggertsson <hallgrimurv@gmail.com>
2024-06-11 19:10:28 +0000

    working on benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -38,40 +38,31 @@ fn my_benchmark(c: &mut Criterion) {
 
     // // concurrent connections
     // c.bench_function("concurrent downloads (small csv)", |b| b.iter( || test_concurrent_connections()));
-    let mut group = c.benchmark_group("smaller sample");
-    // group.bench_function("ColumnTable::from_csv_string, 1.000.000 random lines", |b| b.iter(|| ColumnTable::from_csv_string(&bench_csv, "bench_test", "criterion")));
+    let mut group = c.benchmark_group("All benchmarks");
     // group.bench_function("StrictTable::from_csv_string, 1.000.000 random lines", |b| b.iter(|| StrictTable::from_csv_string(&bench_csv, "bench_test")));
     
     // let address = "127.0.0.1:3004";
     // let username = "admin";
     // let password = "admin";
     // group.bench_function("upload_table", |b| b.iter(|| upload_table(address, username, password, "large_csv", &bench_csv)));
+    
+    let google_docs_csv = std::fs::read_to_string(format!("test_files{PATH_SEP}test_csv_from_google_sheets_combined_sorted.csv")).unwrap();
+    group.bench_function("EZTable::from_csv_string, 1.000.000 random lines", |b| b.iter(|| EZTable::from_csv_string(&google_docs_csv, "bench_test", "criterion")));
 
-    let input = std::fs::read_to_string(format!("test_files{PATH_SEP}test_csv_from_google_sheets_combined_sorted.csv")).unwrap();
-    let t = EZTable::from_csv_string(&input, "test", "test").unwrap();
+    let t = EZTable::from_csv_string(&google_docs_csv, "test", "test").unwrap();
+    
+    group.bench_function("Write to raw binary", |b| b.iter(|| t.write_to_raw_binary()));
+    group.bench_function("Write to csv string", |b| b.iter(|| t.to_string()));
+
     let bint_t = t.write_to_raw_binary();
     let string_t = t.to_string();
-    println!("bin_t lent: {}", bint_t.len());
-    println!("string_t lent: {}", string_t.len());
-    // group.bench_function("binary", |b| b.iter(|| EZTable::read_raw_binary(&bint_t)));
-    group.bench_function("csv", |b| b.iter(|| EZTable::from_csv_string(&string_t, "test", "test")));
-
-    group.bench_function("compress string miniz_oxide", |b| b.iter(|| miniz_compress(string_t.as_bytes())));
-    // group.bench_function("compress string brotli", |b| b.iter(|| brotli_compress(string_t.as_bytes())));
-
-    let comp_mo_string_t = miniz_compress(string_t.as_bytes()).unwrap();
-    // let comp_br_string_t = brotli_compress(string_t.as_bytes());
-
-    group.bench_function("decompress string miniz_oxide", |b| b.iter(|| miniz_decompress(&comp_mo_string_t)));
-    // group.bench_function("decompress string brotli", |b| b.iter(|| brotli_decompress(&comp_br_string_t)));
+    
+    group.bench_function("read from raw binary", |b| b.iter(|| EZTable::read_raw_binary("bench", &bint_t)));
+    group.bench_function("read from csv string", |b| b.iter(|| EZTable::from_csv_string(&string_t, "bench2", "criterion")));
 
 
-    // StrictTable::from_csv_string
-    // group.sample_size(10);
-    // group.bench_function("StrictTable::from_csv_string, big", |b| b.iter(|| StrictTable::from_csv_string(&big_csv, "good_csv")));
-    // group.sample_size(100);
-    // group.bench_function("StrictTable::from_csv_string, small", |b| b.iter(|| StrictTable::from_csv_string(&good_csv, "good_csv")));
-}
+
+    }
 
 criterion_group!(benches, my_benchmark);
 criterion_main!(benches);
